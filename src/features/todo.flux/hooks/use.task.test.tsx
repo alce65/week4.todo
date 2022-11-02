775a7cf3e7272efa2956b545677b324ecb28552a
Alejandro Cerezo <alce65@hotmail.es>
2022-11-02 12:24:16 +0100

    Update useTask test

--- src/features/todo.flux/hooks/use.task.test.tsx
+++ src/features/todo.flux/hooks/use.task.test.tsx
@@ -1,4 +1,47 @@
-import { renderHook } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
+import { ITask, Task } from '../models/task';
 import { useTasks } from './use.task';
-const { result } = renderHook(() => useTasks());
-console.log(result.current);
+import { TaskApi } from '../services/task.api';
+
+jest.mock('../services/task.api');
+
+const task1 = {
+    title: 'Added task',
+    responsible: '',
+    isComplete: false,
+};
+const newTask = {
+    title: 'Added task',
+    responsible: '',
+    isComplete: false,
+};
+
+describe('Given', () => {
+    let result: {
+        current: {
+            tasks: Array<Task>;
+            handleAdd: (newTask: ITask) => void;
+            handlerEraser: (task: Task) => void;
+            handlerUpdate: (updateTask: Task) => void;
+        };
+    };
+
+    beforeEach(() => {
+        TaskApi.prototype.getTasks = jest.fn().mockResolvedValue([task1]);
+        TaskApi.prototype.createTask = jest.fn().mockResolvedValue(newTask);
+        ({ result } = renderHook(() => useTasks()));
+    });
+
+    test('should first', async () => {
+        await waitFor(() => {
+            expect(result.current.tasks).toEqual([task1]);
+        });
+    });
+
+    test('should first b', async () => {
+        await waitFor(() => {
+            result.current.handleAdd(newTask);
+            expect(result.current.tasks.at(-1)).toEqual(newTask);
+        });
+    });
+});
